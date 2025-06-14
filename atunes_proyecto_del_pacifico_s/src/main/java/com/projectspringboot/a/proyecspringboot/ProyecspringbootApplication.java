8eb596f2b46cc6f00abed1cc7bcb81e7ec0db8e0
SebaGut2103 <juansefernando.guti2103@gmail.com>
2025-06-13 19:06:47 -0500

    Stating the springboot project

---
+++ atunes_proyecto_del_pacifico_s/src/main/java/com/projectspringboot/a/proyecspringboot/ProyecspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.atunes_proyecto_del_pacifico_s.a.proyecspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyecspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyecspringbootApplication.class, args);
+	}
+
+}
